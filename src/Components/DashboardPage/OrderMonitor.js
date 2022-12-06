30c345c52de356c1ccd9e23bd29f0a4d8c5c0a4a
JaznanOfficial <jaznanofficial@gmail.com>
2022-12-06 08:40:50 +0600

    more update

--- src/Components/DashboardPage/OrderMonitor.js
+++ src/Components/DashboardPage/OrderMonitor.js
@@ -45,7 +45,7 @@ const OrderMonitor = () => {
                                 <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-green-100 text-green-500">
                                 <i class="fa-solid fa-check-double"></i>
                                 </div>
-                                <div class="flex flex-col justify-start ml-4">
+                                <div class="flex flex-col flex-grow ml-4">
                                     <div class="text-sm text-gray-500">Order Complete</div>
                                     <div class="font-bold text-lg">32</div>
                                 </div>
