f9c8a0133fdf7fd94fe5c6fe775d4082685f8440
Nijumdip <nijumdip019911@gmail.com>
2022-12-15 11:11:51 +0600

    cupon modal done

--- src/Components/BrandsPage/BrandTopBar.js
+++ src/Components/BrandsPage/BrandTopBar.js
@@ -50,7 +50,6 @@ const BrandTopBar = () => {
                                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                         {/*header*/}
                                         <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                            
                                             <button
                                                 className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                                 onClick={() => setShowModal(false)}
@@ -168,7 +167,6 @@ const BrandTopBar = () => {
                                                             </div>
                                                         </div>
                                                     </div> */}
-                                                    
                                                 </div>
                                             </div>
                                         </div>
