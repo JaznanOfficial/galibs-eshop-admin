efd406a302df8ec38cc9618e5db70a9fa5d595fc
Nijumdip <nijumdip019911@gmail.com>
2022-12-15 11:29:29 +0600

    cupon page comlete

--- src/Components/CouponsPage/CouponTopBar.js
+++ src/Components/CouponsPage/CouponTopBar.js
@@ -194,13 +194,6 @@ const CouponTopBar = () => {
                                                 Close
                                             </button>
                                             <button
-                                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                                type="button"
-                                            >
-                                                Save Changes
-                                            </button>
-
-                                            <button
                                                 className="btn w-full md:w-1/5 bg-primary text-white hover:bg-white hover:text-primary hover:border-primary"
                                                 onClick={() => setShowModal(false)}
                                             >
