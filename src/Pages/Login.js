56eeeb9cf270dc6c397e0325e54e7753fcbc8a0f
Nijumdip <nijumdip019911@gmail.com>
2022-12-15 21:16:37 +0600

    bakita nije kore niyen    you just broke my heart

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { FaMoneyCheckAlt } from "react-icons/fa";
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
     return (
         <>
-            <div className="w-full px-5 md:px-0 my-5 flex justify-center items-center">
-                <div className="md:w-11/12 w-full  mx-auto flex justify-center items-center">
-                    <div className="card md:w-96 w-full h-96 bg-emerald-500 shadow-xl md:my-0">                       
+            <div className="w-full px-5 md:px-0 flex justify-center items-center">
+                <div className="md:w-11/12 w-full h-screen mx-auto flex justify-center items-center">
+                    <div className="card md:w-96 h-96 bg-emerald-200 shadow-xl ">
                         <div className="card-body ">
                             <div className="flex flex-col justify-center items-center text-white">
-                                <img src="" alt="" srcset="" />
-                                <h1 className="card-title">
-                                    <FaMoneyCheckAlt className=" text-5xl" />
-                                </h1>
-                                <h2 className="text-xl my-2">Today's Order</h2>
-                                <h3 className="text-5xl font-extrabold">৳ 35000</h3>
+                                <img
+                                    className="w-52 h-52 rounded-full"
+                                    
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc64rCnCx-jAUwswI3CHx8diq5psB3lbrHbr1RNhb9MFZkvD7-0n9qPhhsZEUIFoLCCkk&usqp=CAU"
+                                    alt=""
+                                />
 
-                                <div className=" w-full m-20">
+                                <div className=" w-full mt-10">
                                     <button
-                                    className="input w-full focus:bg-white bg-green-100 text-gray-400"
+                                        className="input w-full focus:bg-white bg-green-100 text-gray-400 flex flex-row justify-center items-center"
                                         // onClick={() => setShowModal(false)}
-                                    >                               
-                                        <span className="ml-1">Login</span>
+                                    >
+                                        <FcGoogle />
+                                        <span className="ml-5">Login With Google</span>
                                     </button>
                                 </div>
                             </div>
