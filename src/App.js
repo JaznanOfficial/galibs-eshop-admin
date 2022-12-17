d82b5793bc7c638525534dbefdb677ebd5c206b5
JaznanOfficial <jaznanofficial@gmail.com>
2022-12-17 18:36:59 +0600

    firebase error solved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import { Route, Routes } from "react-router-dom";
+import { Bars } from "react-loader-spinner";
 import "./App.css";
 import Navigation from "./Components/Navigation/Navigation";
 import RoutesComponents from "./Components/Routes/RoutesComponents";
@@ -7,9 +8,23 @@ import Login from "./Pages/Login";
 // import DashboardPage from "./Pages/DashboardPage";
 
 function App() {
-    const { user } = useFirebase();
+    const { user, loading } = useFirebase();
     console.log(user);
-    if (!user.auth) {
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center w-screen h-screen">
+                <Bars
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="bars-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                />
+            </div>
+        );
+    } else if (!user.auth) {
         return (
             <div className="App ">
                 <Login />
