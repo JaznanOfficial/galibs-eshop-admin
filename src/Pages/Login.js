22c9430400c03cc0c7b0172aba19a152ec251562
JaznanOfficial <jaznanofficial@gmail.com>
2022-12-17 09:33:01 +0600

    logint done

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -6,19 +6,19 @@ const Login = () => {
         <>
             <div className="w-full px-5 md:px-0 flex justify-center items-center">
                 <div className="md:w-11/12 w-full h-screen mx-auto flex justify-center items-center">
-                    <div className="card md:w-96 h-96 bg-emerald-200 shadow-xl ">
+                    <div className="card md:w-96 h-96 bg-emerald-400 shadow-xl ">
                         <div className="card-body ">
                             <div className="flex flex-col justify-center items-center text-white">
                                 <img
                                     className="w-52 h-52 rounded-full"
                                     
-                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc64rCnCx-jAUwswI3CHx8diq5psB3lbrHbr1RNhb9MFZkvD7-0n9qPhhsZEUIFoLCCkk&usqp=CAU"
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1WI0fEmhyWbKPf37f3uMMvc3WUmN9sqJG0g&usqp=CAU"
                                     alt=""
                                 />
 
                                 <div className=" w-full mt-10">
                                     <button
-                                        className="input w-full focus:bg-white bg-green-100 text-gray-400 flex flex-row justify-center items-center"
+                                        className="input w-full bg-white hover:bg-green-50 hover:shadow-xl text-gray-600 flex flex-row justify-center items-center"
                                         // onClick={() => setShowModal(false)}
                                     >
                                         <FcGoogle />
