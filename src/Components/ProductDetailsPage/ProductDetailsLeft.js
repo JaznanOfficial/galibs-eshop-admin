07b0d99256ae29df7ebfbcf45755232739b7f555
Nijumdip <nijumdip019911@gmail.com>
2022-12-26 09:04:41 +0600

    added  extra images components

--- src/Components/ProductDetailsPage/ProductDetailsLeft.js
+++ src/Components/ProductDetailsPage/ProductDetailsLeft.js
@@ -2,18 +2,44 @@ import React from "react";
 
 const ProductDetailsLeft = () => {
     return (
-        <>
+        <div >
             
-                <div class="w-52  md:w-1/2 px-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center sm:mx-2 mx-12  ">
-                    <img
-                        src="https://media.istockphoto.com/id/1186325137/video/mens-hands-are-typing-on-the-laptop-keyboard-close-up.jpg?s=640x640&k=20&c=C_7_hQp7j472bs7tSN1ghMDoOi2U8gQqz7VxnkQnHFY="
-                        class="w-80 h-80 my-32"
-                        alt=""
-                    />
-                </div>
+            <div class="sm:w-full  md:w-11/12 px-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center sm:mx-2 mx-12  ">
+                <img
+                    src="https://media.istockphoto.com/id/1186325137/video/mens-hands-are-typing-on-the-laptop-keyboard-close-up.jpg?s=640x640&k=20&c=C_7_hQp7j472bs7tSN1ghMDoOi2U8gQqz7VxnkQnHFY="
+                    class="h-80 my-32"
+                    alt=""
+                />
+            </div>
+            
+            <div className="flex sm:flex-row flex-col  gap-2 justify-center items-center  mt-5">
+                        <div class="w-28 p-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center ">
+                            <img
+                                src="https://media.istockphoto.com/id/1186325137/video/mens-hands-are-typing-on-the-laptop-keyboard-close-up.jpg?s=640x640&k=20&c=C_7_hQp7j472bs7tSN1ghMDoOi2U8gQqz7VxnkQnHFY="
+                                class="w-20 h-20 "
+                                alt=""
+                            />
+                        </div>
+
+                        <div class="w-28 p-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center ">
+                            <img
+                                src="https://media.istockphoto.com/id/1186325137/video/mens-hands-are-typing-on-the-laptop-keyboard-close-up.jpg?s=640x640&k=20&c=C_7_hQp7j472bs7tSN1ghMDoOi2U8gQqz7VxnkQnHFY="
+                                class="w-20 h-20 "
+                                alt=""
+                            />
+                        </div>
+
+                        <div class="w-28 p-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center ">
+                            <img
+                                src="https://media.istockphoto.com/id/1186325137/video/mens-hands-are-typing-on-the-laptop-keyboard-close-up.jpg?s=640x640&k=20&c=C_7_hQp7j472bs7tSN1ghMDoOi2U8gQqz7VxnkQnHFY="
+                                class="w-20 h-20 "
+                                alt=""
+                            />
+                        </div>
+                    </div>
                 
             
-        </>
+        </div>
     );
 };
 
