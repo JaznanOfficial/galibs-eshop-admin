54ce08586816b723cc21acf609a1ec046b39ae3a
Nijumdip <nijumdip019911@gmail.com>
2023-01-02 18:55:42 +0600

    changes some style

--- src/Components/ProductDetailsPage/ProductDetailsLeft.js
+++ src/Components/ProductDetailsPage/ProductDetailsLeft.js
@@ -4,9 +4,9 @@ const ProductDetailsLeft = ({location}) => {
     console.log(location);
     const { img } = location.state;
     return (
-        <div className="sm:-mr-12 -ml-0" >
+        <div className="mr-10 sm:ml-20 ml-0" >
             
-            <div class="sm:w-full  md:w-10/12 px-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center sm:mx-2 mx-5  ">
+            <div class="w-full px-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center sm:mx-2 mx-5  ">
                 {/* <img
                     src="https://media.istockphoto.com/id/1186325137/video/mens-hands-are-typing-on-the-laptop-keyboard-close-up.jpg?s=640x640&k=20&c=C_7_hQp7j472bs7tSN1ghMDoOi2U8gQqz7VxnkQnHFY="
                     class="h-80 my-32"
@@ -15,12 +15,12 @@ const ProductDetailsLeft = ({location}) => {
 
                 <img
                     src={img}
-                    class="h-80 my-32"
+                    class="w-full h-80 my-32"
                     alt=""
                 />
             </div>
             
-            <div className="flex sm:flex-row flex-col  gap-2 justify-center items-center sm:mr-20 mr-0  mt-5">
+            <div className="flex sm:flex-row flex-col gap-2 justify-center items-center sm:mr-16 mr-0 ml-20  mt-5">
                         <div class="w-28 p-5 mb-10 md:mb-0 border-2 border-green-200 flex justify-center items-center ">
                             <img
                                 src="https://media.istockphoto.com/id/1186325137/video/mens-hands-are-typing-on-the-laptop-keyboard-close-up.jpg?s=640x640&k=20&c=C_7_hQp7j472bs7tSN1ghMDoOi2U8gQqz7VxnkQnHFY="
