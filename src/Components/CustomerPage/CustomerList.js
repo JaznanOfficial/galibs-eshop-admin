c290aec7e0729c1604ec874fce981bec2ccdfb09
JaznanOfficial <jaznanofficial@gmail.com>
2023-01-03 11:59:28 +0600

    customer data loading added

--- src/Components/CustomerPage/CustomerList.js
+++ src/Components/CustomerPage/CustomerList.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import CustomerSingleList from "./CustomerSingleList";
 import { CustomersContext } from "../../Pages/CustomersPage";
+import { Watch } from "react-loader-spinner";
 
 
 const CustomerList = () => {
@@ -12,6 +13,20 @@ const CustomerList = () => {
                 <div className="  flex items-top justify-center  font-sans overflow-auto">
                     <div className="w-11/12 mx-auto">
                         <div className="w-auto overflow-auto bg-white shadow-md rounded">
+                        {loading ? (
+                            <div className="flex justify-center items-center w-full mx-auto py-10">
+                                <Watch
+                                    height="80"
+                                    width="80"
+                                    color="#4fa94d"
+                                    ariaLabel="bars-loading"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                    visible={true}
+                                    style={{ margin: "0 auto" }}
+                                />
+                            </div>
+                        ) : (
                             <table className="w-full table-auto">
                                 <thead>
                                     <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
@@ -44,6 +59,8 @@ const CustomerList = () => {
                                         })}
                                     </tbody>
                             </table>
+                        )}
+                            
                         </div>
                     </div>
                 </div>
