0ca8ae321d48071ce3b2b6a046881867bf38cc45
JaznanOfficial <jaznanofficial@gmail.com>
2023-01-06 17:33:31 +0600

    order context added

--- src/Pages/OrdersPage.js
+++ src/Pages/OrdersPage.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import OrdersList from '../Components/OrdersPage/OrdersList';
-import OrdersTopBar from '../Components/OrdersPage/OrdersTopBar';
+import React, { createContext, useEffect } from "react";
+import OrdersList from "../Components/OrdersPage/OrdersList";
+import OrdersTopBar from "../Components/OrdersPage/OrdersTopBar";
+import useFetch from "../Hooks/useFetch";
+
+const OrdersContext = createContext();
 
 const OrdersPage = () => {
+    const { getData, data, loading } = useFetch();
+
+    useEffect(() => {
+        getData("https://g-shop-server.onrender.com/api/v1/users");
+    }, []);
+    console.log(data);
     return (
-        <div className="py-10">
-            <div>
-                <h1 className="text-2xl  font-extrabold text-black">Orders</h1>
+        <OrdersContext.Provider value={{ data, loading }}>
+            <div className="py-10">
+                <div>
+                    <h1 className="text-2xl  font-extrabold text-black">Orders</h1>
+                </div>
+                <OrdersTopBar />
+                <OrdersList />
             </div>
-            <OrdersTopBar />
-            <OrdersList />
-        </div>
+        </OrdersContext.Provider>
     );
 };
 
